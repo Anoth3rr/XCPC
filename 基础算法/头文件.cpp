4bf8d650e13c31b4eb9e434cbb4dd326d469a129
Anoth3r <113284964+Anoth3rr@users.noreply.github.com>
2025-11-04 21:45:20 +0800

    Update 头文件.cpp

--- 基础算法/头文件.cpp
+++ 基础算法/头文件.cpp
@@ -40,7 +40,7 @@ void solve() {
 
 int main() {
     ios::sync_with_stdio(0);
-    cin.tie(0); cout.tie(0);
+    cin.tie(0);
 
     init();
 
@@ -50,4 +50,5 @@ int main() {
         solve();
     }
     return 0;
+
 }
